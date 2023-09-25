bb5b059abe39a880ea56af3d13a4750552536a4c
eunaejang <dmsdo1234@gmail.com>
2023-09-25 17:34:01 +0900

    간편 로그인 UI 적용

--- MoA/Sources/Scenes/SigeIn/SignInViewController.swift
+++ MoA/Sources/Scenes/SigeIn/SignInViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 import Foundation
+import SafariServices
 
-class SignInViewController: UIViewController {
+//let kCloseSafariViewControllerNotification = "kCloseSafariViewControllerNotification"
+
+class SignInViewController: UIViewController, UIViewControllerTransitioningDelegate, SFSafariViewControllerDelegate {
 
     @IBOutlet weak var googleButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //NotificationCenter.default.addObserver(self, selector: "safariLogin", name: kCloseSafariViewControllerNotification, object: nil)
     }
     
     @IBAction func googleButtonDidTap(_ sender: UIButton) {
@@ -22,6 +27,7 @@ class SignInViewController: UIViewController {
         urlComponents.scheme = "https"
         urlComponents.host = "port-0-moa-spring-3prof2llkqnph83.sel4.cloudtype.app"
         urlComponents.path = "/oauth2/authorize/google"
+        urlComponents.query = "requester=APP"
 
         // URL 구성 요소를 사용하여 URL 생성
         guard let url = urlComponents.url else { return }
@@ -40,11 +46,14 @@ class SignInViewController: UIViewController {
                 print("Error: \(error)")
             } else if let data = data {
                 // HTML 데이터 가져오기
-                let urlContent = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
-                print(url)
+                //let urlContent = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
                 // Safari로 띄우기
+                let safariVC = SFSafariViewController(url: url)
+                // delegate 지정 및 presentation style 설정.
+                safariVC.transitioningDelegate = self
+                safariVC.modalPresentationStyle = .pageSheet
                 DispatchQueue.main.async {
-                    UIApplication.shared.open(url, options: [:])
+                    self.present(safariVC, animated: true, completion: nil)
                 }
             } else {
                 print("Unexpected error")
@@ -54,4 +63,7 @@ class SignInViewController: UIViewController {
         task.resume() // 작업 시작
     }
     
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        self.dismiss(animated: true, completion: nil)
+    }
 }
