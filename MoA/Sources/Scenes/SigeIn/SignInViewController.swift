eb40cbe6ddb3279bea396f1a1202445bd32dc9b1
eunaejang <dmsdo1234@gmail.com>
2023-10-09 19:46:36 +0900

    카카오톡으로 로그인하기

--- MoA/Sources/Scenes/SigeIn/SignInViewController.swift
+++ MoA/Sources/Scenes/SigeIn/SignInViewController.swift
@@ -7,63 +7,54 @@
 
 import UIKit
 import Foundation
-import SafariServices
+import KakaoSDKUser
 
-//let kCloseSafariViewControllerNotification = "kCloseSafariViewControllerNotification"
-
-class SignInViewController: UIViewController, UIViewControllerTransitioningDelegate, SFSafariViewControllerDelegate {
-
-    @IBOutlet weak var googleButton: UIButton!
+class SignInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //NotificationCenter.default.addObserver(self, selector: "safariLogin", name: kCloseSafariViewControllerNotification, object: nil)
+    }
+    
+    @IBAction func kakaoButtonDidTap(_ sender: UIButton) {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+                    
+                    //do something
+                    _ = oauthToken
+                    
+                    self.setUserInfo()
+                }
+            }
+        } else {
+            print("안됨")
+        }
     }
     
     @IBAction func googleButtonDidTap(_ sender: UIButton) {
-        // URLComponents를 사용하여 URL 구성
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "port-0-moa-spring-3prof2llkqnph83.sel4.cloudtype.app"
-        urlComponents.path = "/oauth2/authorize/google"
-        urlComponents.query = "requester=APP"
-
-        // URL 구성 요소를 사용하여 URL 생성
-        guard let url = urlComponents.url else { return }
-        
-        // URLRequest 인스턴스 생성
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST" // 요청에 사용할 HTTP 메서드 설정
-
-        // HTTP 헤더 설정
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        
-        // URLSession을 사용하여 요청 수행
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                print("Error: \(error)")
-            } else if let data = data {
-                // HTML 데이터 가져오기
-                //let urlContent = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
-                // Safari로 띄우기
-                let safariVC = SFSafariViewController(url: url)
-                // delegate 지정 및 presentation style 설정.
-                safariVC.transitioningDelegate = self
-                safariVC.modalPresentationStyle = .pageSheet
-                DispatchQueue.main.async {
-                    self.present(safariVC, animated: true, completion: nil)
-                }
-            } else {
-                print("Unexpected error")
-            }
-        }
-
-        task.resume() // 작업 시작
     }
     
-    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-        self.dismiss(animated: true, completion: nil)
+    func setUserInfo() {
+        UserApi.shared.me() {(user, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("me() success.")
+                //do something
+                _ = user
+                /*self.infoLabel.text = user?.kakaoAccount?.profile?.nickname
+                
+                if let url = user?.kakaoAccount?.profile?.profileImageUrl,
+                   let data = try? Data(contentsOf: url) {
+                    self.profileImageView.image = UIImage(data: data)
+                }*/
+            }
+        }
     }
 }
